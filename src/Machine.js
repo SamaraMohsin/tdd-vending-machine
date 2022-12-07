179a2f94be22409a3a4e3ccd4658621dcad015d5
Hafiza-Samara-Mohsin <mohsin4005617@cloud.neduet.edu.pk>
2022-12-07 21:58:24 +0500

    4th AC refactored and failed

--- src/Machine.js
+++ src/Machine.js
@@ -20,33 +20,7 @@ module.exports = class Machine {
     }
 
     // 4TH CASE
-    selectItem(code){
-            // 1 for crisps 
-            // 2 for chocolate
-            // 3 for mints
-            if(code === 1){
-                if (this.total_deposit < 100){
-                    let mymoney =  Math.abs(ths.items[0]['crisps']-this.total_deposit);
-                    return `Your deposit is insufficient.  Please add ${mymoney} for this item`
-                }
-                else{
-                    return {item: 'mints', change: [20, 10]}
-                }
-
-            }
-            else if(code === 2){
-                return('Chocolate are unavailable')
-
-            }
-            else if(code === 3){
-                if (this.total_deposit < 70){
-                    let mymoney =  Math.abs(this.items[2]['mints']-this.total_deposit);
-                    return `Your deposit is insufficient.  Please add Rs ${mymoney} for this item`
-                }
-
-            }
-
-        }
+    // 
 
 
         
--- test/Machine.test.js
+++ test/Machine.test.js
@@ -1,92 +1,87 @@
-const Machine = require('../src/Machine');
+const Machine = require("../src/Machine");
 
-describe('the vending machine', () => {
-    // 1st AC
-    it('should have items to purchase', () => {
-        // setup
-        const machine = new Machine();
-        const expected = [{'crisps': 100}, {'chocolate': 350}, {'mints': 70}];
+describe("the vending machine", () => {
+  // 1st AC
+  it("should have items to purchase", () => {
+    // setup
+    const machine = new Machine();
+    const expected = [{ crisps: 100 }, { chocolate: 350 }, { mints: 70 }];
 
-        // exercise
-        const actual = machine.seeSelections();
+    // exercise
+    const actual = machine.seeSelections();
 
-        // assert
-        expect(expected).toEqual(actual);
-    });
+    // assert
+    expect(expected).toEqual(actual);
+  });
 
-    // 2nd AC
-    it('Display money and Show the menu accordingly', () => {
-        // setup
-        const machine = new Machine();
-        const money = 100;
-        const expected = "You have deposited Rs " + money;
+  // 2nd AC
+  it("Display money and Show the menu accordingly", () => {
+    // setup
+    const machine = new Machine();
+    const money = 100;
+    const expected = "You have deposited Rs " + money;
 
-        // exercise
-        const actual =  machine.deposit(money);
+    // exercise
+    const actual = machine.deposit(money);
 
-        // assert
-        expect(expected).toEqual(actual);
-    });
+    // assert
+    expect(expected).toEqual(actual);
+  });
 
-    // 3rd AC
-    it('Additional Money', () => {
-        // setup
-        const machine = new Machine();
-        const money= 100;
-        machine.deposit(money)
-        // machine.deposit(50)
-        const actual = "You have deposited Rs 150"
-        // exercise
-        const expected =  machine.deposit(50);
+  // 3rd AC
+  it("Additional Money", () => {
+    // setup
+    const machine = new Machine();
+    const money = 100;
+    machine.deposit(money);
+    // machine.deposit(50)
+    const actual = "You have deposited Rs 150";
+    // exercise
+    const expected = machine.deposit(50);
 
-        // assert
-        expect(expected).toEqual(actual);
-    });
+    // assert
+    expect(expected).toEqual(actual);
+  });
 
-    // 4th AC
-    it('Unavailable Item', () => {
-        // setup
-        const machine = new Machine();
-        const actual =  'Chocolate are unavailable';
-        const code = 2;
+  // 4th AC
+  it("Unavailable Item", () => {
+    // setup
+    const machine = new Machine();
+    machine.items.push({}); //I am creating a setup where I am assuming that I have an unavailable item in my list
+    const actual = "The item you selected is unavailable";
+    // exercise
+    const expected = machine.selectItem(3);
+    // assert
+    expect(expected).toEqual(actual);
+  });
 
-        // exercise
-        const expected =  machine.selectItem(code);
+  // 5th Case
+  // it('Insufficient Balance', () => {
+  //     // setup
+  //     const machine = new Machine();
+  //     const actual =  'Your deposit is insufficient.  Please add Rs 20 for this item';
+  //     const code = 3;
+  //     machine.deposit(50)
 
-        // assert
-        expect(expected).toEqual(actual);
-    });
+  //     // exercise
+  //     const expected =  machine.selectItem(code);
 
-    // 5th Case
-    it('Insufficient Balance', () => {
-        // setup
-        const machine = new Machine();
-        const actual =  'Your deposit is insufficient.  Please add Rs 20 for this item';
-        const code = 3;
-        machine.deposit(50)
+  //     // assert
+  //     expect(expected).toEqual(actual);
+  // });
 
-        // exercise
-        const expected =  machine.selectItem(code);
+  // 6th Case
+  //  it('Return Change', () => {
+  //     // setup
+  //     const machine = new Machine();
+  //     const actual =  {item: 'mints', change: [20, 10]};
+  //     const code = 3;
+  //     machine.deposit(100)
 
-        // assert
-        expect(expected).toEqual(actual);
-    });
+  //     // exercise
+  //     const expected =  machine.selectItem(code);
 
-     // 6th Case
-     it('Return Change', () => {
-        // setup
-        const machine = new Machine();
-        const actual =  {item: 'mints', change: [20, 10]};
-        const code = 3;
-        machine.deposit(100)
-
-        // exercise
-        const expected =  machine.selectItem(code);
-
-        // assert
-        expect(expected).toEqual(actual);
-    });
-
-
-
+  //     // assert
+  //     expect(expected).toEqual(actual);
+  // });
 });
