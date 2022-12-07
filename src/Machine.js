d4155e01b84707e61e5d0223796a7d8a307272df
Hafiza-Samara-Mohsin <mohsin4005617@cloud.neduet.edu.pk>
2022-12-07 22:53:38 +0500

    4th AC passed and refactored

--- src/Machine.js
+++ src/Machine.js
@@ -2,26 +2,33 @@ let var1;
 // let price1 =70;
 // let price2 =100;
 // let proce3 =350;
-module.exports = class Machine {
-    constructor() {
-        this.items = [{'crisps': 100}, {'chocolate': 350}, {'mints': 70}];
-        this.total_deposit = 0;
+class Machine {
+  constructor() {
+    this.items = [{ crisps: 100 }, { chocolate: 350 }, { mints: 70 }];
+    this.total_deposit = 0;
+  }
 
+  seeSelections() {
+    return this.items;
+  }
+
+  // 2nd AC, 3rd AC
+  deposit(denomination) {
+    this.total_deposit += denomination;
+    return `You have deposited Rs ${this.total_deposit}`;
+  }
+
+  // 4TH CASE
+  selectItem(code) {
+    this.items.push(0);
+    if (JSON.stringify(this.items[code]) === "{}") {
+      return "The item you selected is unavailable";
     }
-    
-    seeSelections() {
-        return this.items;
-    }
+  }
+}
 
-    // 2nd AC, 3rd AC
-    deposit(denomination){
-        this.total_deposit += denomination;
-        return `You have deposited Rs ${this.total_deposit}`;
-    }
+module.exports = Machine;
 
-    // 4TH CASE
-    // 
-
-
-        
-};
+// ob = new Machine();
+// ob.items.push({});
+// console.log(ob.items);
